a922cf8582cc0925d25413d9b84b0a1d91b1e063
Pavithra8805 <pavithra8805@gmail.com>
2024-03-19 13:22:46 +0000

    Connects to MongoDB

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
-const mongoose=require('mongoose');
-const express=require('express');
-require('dotenv').config();
+// const mongoose=require('mongoose');
+// const express=require('express');
+// require('dotenv').config();
 
-const port=process.env.PORT || 3008;
+// const port=process.env.PORT || 3008;
 
-const app=express();
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
+// const app=express();
+// mongoose.connect(process.env.MONGODB_URI,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true,
+// })
 
-.then(()=>console.log('Connectedto MongoDB'))
-.catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1); 
+// .then(()=>console.log('Connectedto MongoDB'))
+// .catch((err) => {
+//     console.error('Error connecting to MongoDB:', err);
+//     process.exit(1); 
+// });
+
+// app.get('/',(req,res)=>{
+//     res.json({
+//         message:'Welcome to the HomePage',
+//         database:mongoose.connection.readyState===1?'connected':'disconnected'
+//     });
+// });
+
+// app.listen(port,()=>{
+//     console.log(`🚀 Server is running on port: ${port}`);
+// });
+
+
+// module.exports = app;
+
+
+const express = require("express");
+const connectDB = require("./config/db.js");
+const { filmRoute } = require('./routes.js');
+connectDB();
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("pong");
 });
 
-app.get('/',(req,res)=>{
-    res.json({
-        message:'Welcome to the HomePage',
-        database:mongoose.connection.readyState===1?'connected':'disconnected'
-    });
+
+//routes
+app.use("/api", filmRoute);
+
+app.listen(port, () => {
+    console.log("Server is running on port");
 });
-
-app.listen(port,()=>{
-    console.log(`Server is running on port: ${port}`);
-});
-
-
-module.exports = app;
