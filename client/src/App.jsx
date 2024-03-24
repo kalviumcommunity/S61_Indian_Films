a2968726b868d72d94b779273f4412f377027186
Pavithra8805 <pavithra8805@gmail.com>
2024-03-24 17:51:21 +0000

    Added AddEntityForm component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,43 @@
+// import './App.css';
+// import Home from './Components/Home';
+// import Entity from './Components/Entity';
+// import {Routes,Route} from 'react-router-dom';
+// import data from '../data.json';
+// import AddEntityPage from './Components/AddEntity';
+// import Navbar from './Components/Navbar';
+
+// function App() {
+
+//   return (
+//     <>
+//       {/* <Route path="/" element={<Home />} /> */}
+//       {/* <Entity {...data} /> */}
+//       {/* <AddEntityPage/> */}
+//       <Navbar/>
+//     </>
+//   )
+// };
+
+// export default App;
+
 import './App.css';
 import Home from './Components/Home';
 import Entity from './Components/Entity';
-// import {Routes,Route} from 'react-router-dom';
-import data from '../data.json';
+import AddEntityPage from './Components/AddEntity';
+import Navbar from './Components/Navbar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-      {/* <Route path="/" element={<Home />} /> */}
-      <Entity {...data} />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/entities" element={<Entity />} />
+        <Route path="/add-entity" element={<AddEntityPage />} />
+      </Routes>
     </>
-  )
-};
+  );
+}
 
 export default App;
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="nav-item">
+                <Link to="/" className="nav-link"><strong>Home</strong></Link></div>
+            <div className="nav-item">
+                <Link to="/entities" className="nav-link"><strong>Entities</strong></Link>
+            </div>
+            <div className="nav-item">
+                <Link to="/add-entity" className="nav-link"><strong>Add Entity</strong></Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
