7686f60842990dd4272ba5d1677d8a2b6efc5dee
Pavithra8805 <pavithra8805@gmail.com>
2024-04-03 18:10:39 +0000

    Adding relations between entities

--- client/src/Components/MovieDB.jsx
+++ client/src/Components/MovieDB.jsx
@@ -7,6 +7,8 @@ import './MovieDB.css';
 
 function MovieDB() {
     const [movies, setMovies] = useState([]);
+    const [filteredData, setFilteredData] = useState("");
+    const [CreatedBy, setCreatedBy] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -14,11 +16,21 @@ function MovieDB() {
             .then(response => {
                 console.log(response.data.data);
                 setMovies(response.data.data);
+                const selectedCreatedBy = response.data.data.reduce((curr, item) => {
+                    if (!curr.includes(item.created_by)) {
+                        curr.push(item.created_by);
+                    }
+                    return curr;
+                }, []);
+
+                console.log("Selected Created By:", selectedCreatedBy); // Log the Created By data
+                setCreatedBy(selectedCreatedBy);
             })
             .catch(error => {
                 console.log('Error fetching movies:', error);
             });
     }, []);
+
 
     const handleDelete = (id) => {
         axios.delete(`http://localhost:4006/api/delete/${id}`)
@@ -36,34 +48,59 @@ function MovieDB() {
         navigate("/update/:id", { state: item });
     };
 
+    const handleFilterChange = (e) => {
+        setFilteredData(e.target.value);
+    };
+
+    const filteredEntities = filteredData
+        ? movies.filter((item) => item.created_by === filteredData)
+        : movies;
+
+
     return (
         <div className="movie-list-container">
             <h1 className='movie-list-title'>All Entities</h1>
+            <div className='filter-area'>
+                <select
+                    id="creatorFilter"
+                    value={filteredData}
+                    onChange={handleFilterChange}
+                >
+                    <option value="">All</option>
+                    {CreatedBy.map((creator, index) => (
+                        <option key={index} value={creator}>
+                            {creator}
+                        </option>
+                    ))}
+
+                </select>
+            </div>
 
             {movies.length === 0 ? (
                 <p>No entities to display</p>
             ) : (
                 <ul className="movie-list">
-                    {movies.map(entity => (
+                    {filteredEntities.map(entity => (
                         <div className='movie-item' key={entity._id}>
                             <div className="movie-details">
-                                
-                                    <p className="movie-detail">Rank: {entity.Rank}</p>
-                                    <p className="movie-detail">Title: {entity.Title}</p>
-                                    <p className="movie-detail">Box Office Collection (INR Crores): {entity.BoxOfficeCollection_INRCrores}</p>
-                                    <p className="movie-detail">Primary Languages: {entity.PrimaryLanguages}</p>
-                                    <p className="movie-detail">Release Year: {entity.ReleaseYear}</p>
-                                    <p className="movie-detail">Genre: {entity.Genre}</p>
-                                    <p className="movie-detail">Directors: {entity.Directors}</p>
-                                    <p className="movie-detail">Lead Actors: {entity.LeadActors}</p>
-                                    <p className="movie-detail">Production Companies: {entity.ProductionCompanies}</p>
-                                    <p className="movie-detail">Description: {entity.Description}</p>
-                                
+
+                                <p className="movie-detail">Rank: {entity.Rank}</p>
+                                <p className="movie-detail">Title: {entity.Title}</p>
+                                <p className="movie-detail">Box Office Collection (INR Crores): {entity.BoxOfficeCollection_INRCrores}</p>
+                                <p className="movie-detail">Primary Languages: {entity.PrimaryLanguages}</p>
+                                <p className="movie-detail">Release Year: {entity.ReleaseYear}</p>
+                                <p className="movie-detail">Genre: {entity.Genre}</p>
+                                <p className="movie-detail">Directors: {entity.Directors}</p>
+                                <p className="movie-detail">Lead Actors: {entity.LeadActors}</p>
+                                <p className="movie-detail">Production Companies: {entity.ProductionCompanies}</p>
+                                <p className="movie-detail">Description: {entity.Description}</p>
+                                <p className="movie-detail">Created By: {entity.created_by}</p>
+
                                 <div className="movie-actions">
                                     {/* <Link to={`/update/${entity._id}`} className='update-link'>Update</Link> */}
                                     <button className="update-link" onClick={() => handleUpdate(entity)}>Edit</button>
                                     <button className="delete-button" onClick={() => handleDelete(entity._id)}>Delete</button>
-                                    
+
                                 </div>
                             </div>
                         </div>
